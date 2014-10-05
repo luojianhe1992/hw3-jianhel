ce2eae0723f12dbcefc50770eea93490fe984214
jianhel <luojianhe1992@163.com>
2014-10-05 03:15:04 -0400

    asdfb

---
+++ hw0-jianheluo/src/main/java/DependencyExample.java
@@ -0,0 +1,23 @@
+
+import java.io.StringReader;
+import edu.stanford.nlp.ling.Word;
+import edu.stanford.nlp.process.TokenizerFactory;
+import edu.stanford.nlp.process.PTBTokenizer.PTBTokenizerFactory;
+import edu.stanford.nlp.process.Tokenizer;
+
+
+public class DependencyExample {
+  public static void main(String[] args) {
+    
+    
+    String s = "I am feeling good";
+    
+    TokenizerFactory<Word> factory =
+    PTBTokenizerFactory.newTokenizerFactory();
+    Tokenizer<Word> tokenizer =
+    factory.getTokenizer(new StringReader(s));
+    System.out.println(tokenizer.tokenize());
+  
+  }
+
+}
